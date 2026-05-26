8074b0a7363be6064f151761855b6b93b696191d
Pavitra71 <pavitraparth71@gmail.com>
2026-05-26 23:11:07 +0530

    button in main

--- local_system.py
+++ local_system.py
@@ -1,2 +1,2 @@
 print("Adding new file to the local repository by intialising file by git init")
-print("alloha, ading content in another branch")
+print("alloha, ading content in another branch (add button in main branch)")
